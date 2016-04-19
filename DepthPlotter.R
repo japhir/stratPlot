f2e1a69bfe3556c5fc1ee700bb9df6262c216a76
Ilja Kocken <iljakocken@gmail.com>
2016-04-19 18:36:54 +0200

    Major updates with several new plotting types and cleanup.

--- DepthPlotter.R
+++ DepthPlotter.R
@@ -16,27 +16,74 @@ SuperPlot.default <- function(
     error = numeric(),         # vector of errors
     errortype = "bars",        # plot as bars or as area
     errorcol = adjustcolor("gray", alpha = 0.9),  # colour of error
-    xax = TRUE,                # logical, draw x-axis
-    type = "P",                # default type
-    fillcol = "blue",          # colour of silhouette 
+    xax = 3,                   # draw x-axis, 1 for bot, 3 for top, NA for none
+    type = "o",                # default type
+    pb = "n",                  # plot bars and/or area using "P", "PB" or "B"
+    left = par("usr")[1],      # from where are area and bars are drawn
+    fillcol = "steelblue",     # colour of silhouette 
     ylab = "Depth (mbsf)",     # default ylab
     xlab = "",                 # default xlab xtitle
     ylim = c(max(yvar, na.rm=TRUE), min(yvar, na.rm=TRUE)), # default ylim
     legend = FALSE,             # default no legend
     legendpos = "topright",    # default legend position
     bty = "n",                 # default legend box type 
-    mar = c(2, 5, 5, 2) + 0.1, # default plot margins
+    mar = c(2, 5, 5, 2) + 0.1, # bottom, left, top and right margins
     ...){                      # possible additional plotting/legend parameters
-    # set up plotting margins.
+  # set up plotting margins.
+  if (identical(xax, 1) && identical(mar, c(2, 5, 5, 2) + 0.1))  # if default, override for bot
+    mar <- c(5, 5, 2, 2) + 0.1
   par(mar = mar)
+  
   if (!add) {  # create empty plot
     plot(var, yvar, ylim = ylim, type = "n", xaxt = "n", xlab = "", ylab = ylab, ...)
-    if(xax) 
-      axis(3)
-    if(!is.null(xlab))
-      mtext(xlab, side=3, line=2)
+    if(xax %in% c(1,3)) 
+      axis(xax)
+    if(!is.null(xlab) && xax %in% c(1,3))
+      mtext(xlab, side = xax, line=2)
     if(legend)
       legend(legendpos, legend = legend, bty = bty, ...)
+  }
+  if (pb == "PB" || pb == "BP") { # for polygon and bars
+    area <- TRUE
+    bars <- TRUE
+  } else if (pb == "P") { # polygon
+    area <- TRUE
+    bars <- FALSE
+  } else if (pb == "B") { # bars
+    area <- FALSE
+    bars <- TRUE
+  } else if (pb == "n") {
+    area <- FALSE
+    bars <- FALSE
+  } else warning ("Incorrect pb")
+  
+  if (area) { 
+    if (anyNA(var) || anyNA(yvar)) {
+      message("NAs found in var/yvar, currently ignoring")
+      nona <- data.frame(var = var, yvar = yvar)
+      nona <- nona[complete.cases(nona), ]
+      x <- c(left, nona$var, left)
+      y <- c(nona$yvar[1], nona$yvar, tail(nona$yvar, n = 1))
+      # TODO: create polygons per non-NA region, like we do with errorregion
+      #      enc <- rle(!is.na(var))
+      #     endIdxs <- cumsum(enc$lengths)
+      #    for (i in 1:length(enc$lengths)) {
+      #     if (enc$values[i]) {
+      #      endIdx <- endIdxs[i]
+      #     startIdx <- endIdx - enc$lengths[i] + 1
+      #    subvar <- var[startIdx:endIdx]
+      #  subyvar <- yvar[startIdx:endIdx]
+      #   x <- c(left, subvar, left)
+      # y <- c(subyvar[1], yvar, tail(yvar, n = 1))
+      #}
+      #}
+    } else {
+    # not sure if I want to use the current left or use:
+    # min(var, na.rm = TRUE) - .04 * diff(range(var, na.rm = TRUE))
+      x <- c(left, var, left)
+      y <- c(yvar[1], yvar, tail(yvar, n = 1))
+    }
+    polygon(x = x, y = y, col = fillcol, border = NA)
   }
   if (length(error) > 0) {  # plot errorstuff
     if(errortype == "bars") {
@@ -44,28 +91,8 @@ SuperPlot.default <- function(
     } else if (errortype == "area")
       PlotErrorArea(var, yvar, error, col = errorcol)
   }
-  if (type == "P") {  # the nonstandard polygon type
-    # check for complete cases without building a data.frame
-    if (anyNA(var)) {
-      message("NAs found in var")
-      yvar <- yvar[!is.na(var)]
-      var <- na.omit(var)
-    }
-    if (anyNA(yvar)) {
-      message("NAs found in yvar")
-      var <- var[!is.na(yvar)]
-      yvar <- na.omit(yvar)
-    }
-    # the far left side of the plot
-    left <- par("usr")[1]
-      # or use: min(var, na.rm = TRUE) - .04 * diff(range(var, na.rm = TRUE))
-    polygon(x = c(left, var, left),
-            y = c(yvar[1], yvar, tail(yvar, n = 1)),
-            col = fillcol, border = NA)
-    segments(x0 = rep(left, length(var)),
-             y0 = yvar,
-             x1 = var)
-    type <- "o"
+  if (bars) {
+    segments(x0 = rep(left, length(var)), y0 = yvar, x1 = var)
   }
   points(var, yvar, type = type, ...)  # plot main line
 }
@@ -92,6 +119,7 @@ PlotErrorBars <- function(var, yvar, errorbars,
 
 PlotErrorArea <- function(var, yvar, errorregion, hor = TRUE, col = adjustcolor("gray", .3)) {
   # adds an errorregion to a variable
+  # TODO: still/again? not working with NAs
   if (anyNA(var) | anyNA(yvar)) {
     enc <- rle(!is.na(var))             # calculate amount of non-NA polygons
     endIdxs <- cumsum(enc$lengths)      # lengths of polygons
@@ -106,14 +134,15 @@ PlotErrorArea <- function(var, yvar, errorregion, hor = TRUE, col = adjustcolor(
         
         x <- c(subdat - subsd, rev(subdat + subsd))
         y <- c(subyvar, rev(subyvar))
+        
+        polygon(x = x, y = y, col = col, border = NA)
       }
     }
   } else {
-        x <- c(var - .5 * errorregion, rev(var + .5 * errorregion))
-        y <- c(yvar, rev(yvar))
+    x <- c(var - .5 * errorregion, rev(var + .5 * errorregion))
+    y <- c(yvar, rev(yvar))
+    polygon(x = x, y = y, col = col, border = NA)
   }
-  polygon(x = x, y = y, col = col, border = NA)
-  
 }
 
 # Takes a dataframe of one or multiple variable(s) to  create a (set of) plot(s)
@@ -121,37 +150,37 @@ SuperPlot.data.frame <- function(
     var,                       # a dataframe of variable(s)
     yvar = NULL,               # a vector of depth or age values
     xlab = "",                 # a vector of characters or a list of formulae 
-                # if length(xlab) > 1 gives errors when checking but works
     ylab = "Depth (mbsf)",
-    type = "P",
-    fillcol = "blue",
+    type = "o",
+    pb = "n",                  # allows 'PB', 'P' and 'B' for polygon with bars
+    fillcols = rep("steelblue", ncol(var)),
     oneplot = FALSE,           # logical, if TRUE plot all variables in the same plot
+    stacked = FALSE,           # logical, if TRUE calculate cumulative sum for vars
     sscols = 1:ncol(var),      # specifies columns of var to subset
-    error = numeric(),         # vector of errors to plot
+    error = numeric(),         # vector of errors to plot (note: relative values!)
     errortype = "bars",        # bars or area 
     errorcol = adjustcolor("gray", alpha = 0.9),  # colour of errorbars/region
-    ...){
+    ...) {
   var <- var[, c(sscols)] # subset only columns of interest   
   # parameter validation
-  if(anyNA(var)) { warning("NAs found in var, ignoring") }
   if(!is.null(yvar)){
-    if(anyNA(yvar)) { warning("NAs found in yvar, ignoring") }
     warning("Assuming only variables in dataframe")
   } else { # only var is provided
     # find value that has yvar
     depthcol <- grep("depth", names(var), ignore.case = TRUE, value = TRUE)
+    if (length(depthcol) > 1) warning("multiple depth columns found, using first")
     agecol <- grep("age", names(var), ignore.case = TRUE, value = TRUE)
-    if (length(depthcol) == 1 && length(agecol) == 0)
-      ycol <- depthcol
+    if (length(agecol) > 1) warning("multiple age columns found, using first")
+    if (length(depthcol) == 1 && length(agecol) == 0) 
+      ycol <- depthcol[1]
     if (length(depthcol) == 0 && length(agecol) == 1) {
-      ycol <- agecol
+      ycol <- agecol[1]
       if (ylab == "Depth (mbsf)")
         ylab <- "Age (Ma)"
     }
     if (length(depthcol) > 0 && length(agecol) > 0) {
       # TODO: interactive selection of desired yvar
-      # TODO: check that this is only one column
-      yvar <- var[, agecol]  # for now we just use age if both are available
+      yvar <- var[, agecol[1]]  # for now we just use age if both are available
       if (ylab == "Depth (mbsf)")
         ylab <- "Age (Ma)"
       # omit depth and age
@@ -161,8 +190,7 @@ SuperPlot.data.frame <- function(
     var   <- var[ , !names(var) %in% ycol]
     }
   }
-  # TODO: do the same thing with other parameters that might be different for each
-  # plot, such as fillcol, type, error, errortype, errorcol
+  # TODO: think of a smart way to provide multiple types, pchs, ltys, fillcols, errors etc.
 
   # parsing of xlab
   if(xlab != ""){
@@ -177,40 +205,51 @@ SuperPlot.data.frame <- function(
       xlab <- ""
     }
   }
-  
   # only one variable in the dataframe var
   if(is.null(ncol(var)))
-    return(SuperPlot(var, yvar, xlab = xlab, ylab = ylab, error = error, ...))
+    return(SuperPlot(var, yvar, xlab = xlab, ylab = ylab, error = error, errortype = errortype,
+                     type = type, pb = pb, ...))
   # multiple variables
   # everything in one plot
   invisible(      # hide output, such as lists of NULL from lapply
     if(oneplot){
       rangeofall <- c(min(var, na.rm = TRUE), max(var, na.rm = TRUE))
       # plot the first variable
-      SuperPlot(var[, 1], yvar, type = type, xlim = rangeofall, ylab = ylab,
-                xlab = xlab, error = error, errortype = errortype,
-                errorcol = errorcol, fillcol = fillcol, ...) 
+      SuperPlot(var[, 1], yvar, type = type, pb = pb,
+                xlim = rangeofall, ylab = ylab, xlab = xlab,
+                error = error, errortype = errortype, errorcol = errorcol,
+                fillcol = fillcols[1], ...) 
       # add the other variables
-      lapply(2:ncol(var), function(i) {
-        SuperPlot(var[ , i], yvar, add = TRUE, type = type,
-                  error = error, errortype = errortype,
-                  errorcol = errorcol, fillcol = fillcol, ...)
-      })
+      if (stacked) {
+        lapply(2:ncol(var), function(i) {
+          SuperPlot(rowSums(var[, 1:i], na.rm = TRUE), yvar, add = TRUE, type = type,
+                    pb = pb, error = error, errortype = errortype,
+                    errorcol = errorcol, fillcol = fillcols[i], ...)
+        })
+      } else {
+        lapply(2:ncol(var), function(i) {
+          SuperPlot(var[ , i], yvar, add = TRUE, type = type, pb = pb,
+                    error = error, errortype = errortype,
+                    errorcol = errorcol, fillcol = fillcols[i], ...)
+        })
+      }
       # multiple plots
     } else if(xlab == ""){ 
     # no or wrong xlab 
       lapply(1:ncol(var), function(i) { 
-        SuperPlot(var[ , i], yvar, xlab = names(var)[i], ylab = ylab,
-                     error = error, ...)
+        SuperPlot(var[ , i], yvar, xlab = names(var)[i], ylab = ylab, type = type, pb = pb,
+                  fillcol = fillcols[i], error = error, errortype = errortype, ...)
       })
     } else if(length(xlab) == 1) # multiplot with provided xlab repeated
       lapply(1:ncol(var), function(i) { 
-        SuperPlot(var[ , i], yvar, xlab = xlab, ylab = ylab, error = error, ...)
+        SuperPlot(var[ , i], yvar, xlab = xlab, type = type, ylab = ylab,
+                  error = error, errortype = errortype, pb = pb, fillcol = fillcols[i], ...)
       })
     # multiple xlabs
     else if(length(xlab) == ncol(var))      
       lapply(1:ncol(var), function(i) { 
-        SuperPlot(var[ , i], yvar, xlab = xlab[[i]], ylab = ylab, error = error, ...)
+        SuperPlot(var[ , i], yvar, xlab = xlab[[i]], ylab = ylab, type = type, fillcol = fillcols[i],
+                  error = error, errortype = errortype, pb = pb, ...)
       })
   ) # end of invisible
 }
--- README.md
+++ README.md
@@ -1,18 +1,19 @@
-# DepthPlotter
-The DepthPlotter function allows you to easily create depth profile plots in R. 
+# SuperPlotter (formerly DepthPlotter)
+The SuperPlot function allows you to easily create depth and age profile plots in R. 
 
 ## Usage
 The simplest way to create a single plot is to give depth and var vectors of equal length, 
 and provide a value for `xlab`. Note that if you want to use superscipts and subscripts in 
-the x-axis label, you can do so by providing `xlab` with a formula, such as: `Label~with~nice~formatting~(H[2]*O~m^{-2}~mu*M)`.
+the x-axis label, you can do so by providing `xlab` with a formula. For example: `Label~with~nice~formatting~(H[2]*O~m^{-2}~mu*M)` will result in: Label with nice formatting (H<sub>2</sub>O m<sup>-2</sup> μM).
 
 You can also provide `var` with a matrix or dataframe of multiple variables. `xlab` can 
 now also be entered as vector of characters or a list of formulae.
 
-It is also possible to provide one dataframe/matrix with the depth-values in it. By 
-default, the function assumes that your dataframe has the depth information in the first 
-column. If this is not the case you can specify it with `depthcol`. If you want to easily 
+It is also possible to provide one dataframe/matrix with the depth or age values in it. By default, the function tries to look up the depth and age columns by rowname.
+You can also specify it with `depthcol`. If you want to easily 
 subset your dataframe, you can use `sscols` to provide column indices of the variables of
 interest.
+
+Setting the `pb` variable to `"PB"`, `"P"` or `"B"` sets the plotting type to polygon with bars, polygon or bars respectively.
 
 Other configurations are also possible, just look at the code and see what it does :).
